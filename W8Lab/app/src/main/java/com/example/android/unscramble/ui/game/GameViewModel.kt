c4ad384395b08d53610e45a3929d02a0edec5732
Nubberino <ignatius.sarwono@student.umn.ac.id>
2023-04-27 20:40:24 +0700

    Populate viewmodel and dialogs

--- W8Lab/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ W8Lab/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,12 +4,40 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+
+
+    private fun getNextWord(): Boolean {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
     init{
         Log.d("GameFragment","Gameviewmodel created!")
+        getNextWord()
     }
-    private var score = 0
+    private var _score = 0
+    val Score:Int get() = _score
     private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private var wordsList: MutableList<String> = mutableListOf()
+
+    private lateinit var currentWord: String
+    private lateinit var _currentScrambledWord = "test"
     val currentScrambledWord: String
         get() = _currentScrambledWord
 }
